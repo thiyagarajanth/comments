ed0931c4c2813cc7f5930bfb182b8dce564eff6e
Thiyagarajan <thiyagarajan.thulasi@object-frontier.com>
2018-05-07 17:55:36 +0530

    Fixed : Script and functionn issues

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,54 +1,28 @@
 class CommentsController < ApplicationController
-  
 
-  # GET /comments
-  # GET /comments.json
   def index
-    ids = Comment.find_by_sql("SELECT array_to_string(array_agg(id), ',') as id, count(*) as cnt, 
-to_timestamp(floor((extract('epoch' from created_at) / 300 )) * 300) 
-as interval_alias
-FROM comments GROUP BY interval_alias  order by 1,2").map(&:id)
-    @comments = Comment.where(:id =>ids).order(:id => :desc)
+    @comments = Comment.lists
     @comment = Comment.new
-    p '------this si sthe '
-    p Comment.find_by_sql("SELECT array_to_string(array_agg(id), ',') as id, count(*) as cnt, 
-to_timestamp(floor((extract('epoch' from created_at) / 300 )) * 300) 
-as interval_alias
-FROM comments GROUP BY interval_alias  order by 1,2").map{|x|[x.id, x.cnt,x.interval_alias]}
-
     respond_to do |format|
-      format.html # new.html.erb
-      format.js #{ render 'new_book_row', :format => :html, :layout => false }
-      format.json { render json: @comment }
+      format.html
+      format.js
     end
   end
 
-  # POST /comments
-  # POST /comments.json
   def create
     @comment = Comment.new(comment_params.merge(:ip_address => request.remote_ip))
-    
-    ids = Comment.find_by_sql("SELECT array_to_string(array_agg(id), ',') as id, count(*) as cnt, 
-            to_timestamp(floor((extract('epoch' from created_at) / 300 )) * 300) 
-            as interval_alias
-            FROM comments GROUP BY interval_alias  order by 1,2").map(&:id)    
-    @comments = Comment.where(:id =>ids).order(:id => :desc)
     respond_to do |format|
       if @comment.save
-        format.html { redirect_to @comment, notice: 'Comment was successfully created.' }
-        format.json { render :show, status: :created, location: @comment }
+        @comments = Comment.lists
         format.js
       else
         format.html { render :new }
-        format.json { render json: @comment.errors, status: :unprocessable_entity }
       end
     end
   end
 
   private
-  
-    # Never trust parameters from the scary internet, only allow the white list through.
     def comment_params
-      params.require(:comment).permit(:notes, :ip_address)
+      params.require(:comment).permit(:notes)
     end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,9 @@
 class Comment < ApplicationRecord
-
-	
+	def self.lists
+		ids = find_by_sql("SELECT array_to_string(array_agg(id order by id desc), ',') as id, count(*) as cnt, 
+            to_timestamp(floor((extract('epoch' from created_at) / 300 )) * 300) 
+            as interval_alias
+            FROM comments GROUP BY interval_alias,ip_address  order by 1,2").map(&:id)    
+		where(:id =>ids).order(:id => :desc)
+	end
 end
